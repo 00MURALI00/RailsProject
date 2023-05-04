28e1b28aad77c34d7517ea67f19a0185bbc1531e
00MURALI00 <muraliprasathn24cse@srishakthi.ac.in>
2023-05-04 12:53:24 +0530

    default image for course

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,12 +5,4 @@ class Course < ApplicationRecord
   belongs_to :teacher
   validates :description, length: { minimum: 10, too_short: '%<count>s is too short for a course description' }
   has_one_attached :image, dependent: :destroy
-  before_create :set_default_image
-
-  def set_default_image
-    return if image.attached?
-
-    image.attach(io: File.open(Rails.root.join('Dummy_flag.jpg')), filename: 'Dummy_flag.jpg',
-                 content_type: 'image/jpg')
-  end
 end
